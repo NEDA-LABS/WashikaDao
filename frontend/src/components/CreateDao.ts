7a785de7062f36efa31e056491fca1c1084b9ddf
clint911 <clingyking007@gmail.com>
2024-07-14 07:13:29 +0300

    adding profile functions

--- frontend/src/components/CreateDao.ts
+++ frontend/src/components/CreateDao.ts
@@ -1,4 +1,22 @@
+import { createWalletClient } from "viem";
+import { celoAlfajores } from "viem/chains";
 
 export default function createDao() {
+
+  const client = createWalletClient({
+    account,
+    //passing chain is how viem knows to try serializing tx as cip42 
+    chain: celoAlfajores,
+    transport: http(),
+  })
+
+  client.writeContract({
+    abi.CONTRACT_ABI,
+    address: CONTRACT_ABI_ADDRESS,
+    functionName: "contractMethod",
+    args: [to, parseEther(value)],
+    //set the fee currency on the contract write call 
+    feeCurrency: FEE_CURRENCIES_ALFAJORES["cusd"]
+  })
 
 }
--- frontend/src/components/Profile.ts
+++ frontend/src/components/Profile.ts
@@ -5,21 +5,59 @@
 
 //@actions: request user to connect wallet & get user accounts
 
-import { createWalletClient } from "viem";
+import { useState } from "react";
+import { http, createPublicClient, createWalletClient, custom, formatEther } from "viem";
 import { celo, celoAlfajores } from 'viem/chains';
 import { injected } from "wagmi/connectors";
 
+
 export default function Profile() {
+  const [hideConnectBtn, setHideConnectBtn] = useState(false);
+  const { connect } = useConnect();
+
+  export const contract = getContract({
+    address: clientAddr,
+    abi: wagmiAbi,//TODO: refactor to add abi 
+    client: {
+      public: publicClient,
+      wallet: walletClient,
+    }
+  })
+
+  function checkForMinipay() {
+    if (window.ethereum && window.ethereum.isMinipay)
+      //User is using minipay wallet 
+      setHideConnectBtn(true);
+
+    connect({ connector: injected({ target: "metaMask" }) })
+  }
+
   const walletClient = createWalletClient({
     chain: celo,
     //chain: celoAlfajores for testnet 
     transport: custom(window.ethereum),
   });
-  const clientAddr = await walletClient.getAddresses();
 
-  function checkForMinipay() {
-    if (window.ethereum && window.ethereum.isMinipay)
-      //User is using minipay wallet 
-      connect({ connector: injected({ target: "metaMask" }) })
+  export const publicClient = createPublicClient({
+    chain: mainnet,  //TODO: Add alt chains
+    transport: http(),
+  })
+
+  export async function getClientAddr() {
+    var clientAddr = await walletClient.requestAddresses();
+    return clientAddr;
   }
+
+  //a better way of getting the balance 
+  export async function getClientBal() {
+    //getting balance in WEI 
+    const _clientBal = await publicClient.getBalance({
+      address: clientAddr,
+    })
+    //formating to ether
+    const clientBal = formatEther(_clientBal)
+    return clientBal;
+  }
+
+
 }
