a0ab253bcf148db685c0cf0188e005722e055a42
clint911 <clingyking007@gmail.com>
2024-09-26 04:08:12 +0300

    updating smart contract tests

--- sc/src/FullDaoContract.sol
+++ sc/src/FullDaoContract.sol
@@ -102,9 +102,9 @@ contract FullDaoContract {
      * @param _daoImageUrlHash Hash of the DAO image URL.
      * @param _multiSigAddr Address of the DAO's multi-signature wallet.
      * @param _multiSigPhoneNo Phone number associated with the DAO's multi-signature wallet.
-     * @return A new instance of DaoDetails.
+     * returns A new instance of DaoDetails.
      */
-    
+
     function createDao(
         string memory _daoName,
         string memory _location,
@@ -137,8 +137,11 @@ contract FullDaoContract {
 
         emit DAOCreated(_multiSigAddr, _daoName, _daoDescription);
     }
-
-    // Function to retrieve DAO details by index
+      /**
+     * @dev Function to retrieve DAO details by multi-signature wallet address.
+     * @param _multiSigAddr Address of the DAO's multi-signature wallet.
+     * @return A struct containing the DAO details. If the DAO is not found, it reverts with "DaoNotFoundError".
+     */
     function getDaoByMultiSig(address _multiSigAddr) public view returns (DaoDetails memory) {
         //require the address passed is a multisig 
         for (uint256 i = 0; i < daoList.length;i++) {
@@ -150,17 +153,34 @@ contract FullDaoContract {
     }
    
    //function to set phone number for a given multiSig address 
+    /**
+    * @dev Function to map a phone number to a DAO's multi-signature wallet address.
+    * @param _multiSigAddr Address of the DAO's multi-signature wallet.
+    * @param _phoneNumber Phone number associated with the DAO's multi-signature wallet.
+    * return No return value.
+    */
    function setMsigToPhoneNumber(address _multiSigAddr, uint256 _phoneNumber) public {
         MsigToPhoneNumber[_multiSigAddr] = _phoneNumber;
         PhoneNumberMsigTracker[_phoneNumber] = true;
    }
    
    //function to get the phone number for a given multiSig address 
+    /**
+    * @dev Function to retrieve the phone number associated with a DAO's multi-signature wallet address.
+    * @param _multiSigAddr Address of the DAO's multi-signature wallet.
+    * @return The phone number associated with the given multi-signature wallet address.
+    *         If the multi-signature wallet address is not found, it reverts with "MsigNotFoundError".
+    */
    function getMsigToPhoneNumber(address _multiSigAddr) public view returns (uint256) {
         return MsigToPhoneNumber[_multiSigAddr];
    }
 
     //function to check if a particular phone number is mapped to a multiSig address 
+        /**
+     * @dev Function to check if a particular phone number is mapped to a multiSig address.
+     * @param _phoneNumber The phone number to check.
+     * @return bool Returns true if the phone number is mapped to a multiSig address, false otherwise.
+     */
     function isPhoneNumberMappedToMsig(uint256 _phoneNumber) public view returns (bool) {
         // Iterate through all known multisig addresses
         if (PhoneNumberMsigTracker[_phoneNumber] == true) {
@@ -170,6 +190,11 @@ contract FullDaoContract {
     }
    
    //function to check if a certain address is a multisig 
+   /**
+    * @dev Function to check if a certain address is a multisig.
+    * @param _address The address to check.
+    * @return bool Returns true if the address is a multisig, false otherwise.
+    */
    function isAddrMultiSig(address _address) public view returns (bool) {
         if (MultisigTracker[_address] == true) { 
             return true;//yes its  a multisig 
@@ -179,6 +204,18 @@ contract FullDaoContract {
  
     event MemberAdded(address indexed _memberAddr, string _email, Role _role);
      // Function to add a member to a DAO
+      /**
+     * @dev Function to add a member to a DAO.
+     * @param _memberName Name of the member.
+     * @param _emailAddress Email address of the member.
+     * @param _phoneNumber Phone number of the member.
+     * @param _nationalId National ID of the member.
+     * @param _role Role of the member in the DAO.
+     * @param _userAddress Ethereum address of the member.
+     * @param _daoMultiSigAddr Ethereum address of the DAO's multi-signature wallet.
+     * @param _multiSigPhoneNo Phone number associated with the DAO's multi-signature wallet.
+     * return No return value.
+     */
     function addMember(
         string memory _memberName,
         string memory _emailAddress,
@@ -212,16 +249,34 @@ contract FullDaoContract {
     }
 
       // Function to get all members of a DAO
+          /**
+     * @dev Retrieves all members of a DAO associated with a specific multi-signature wallet address.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @return MemberDetails[] An array of MemberDetails structs representing the members of the DAO.
+     *         If no members are found, an empty array is returned.
+     */
     function getMembers(address _daoMultiSigAddr) public view returns (MemberDetails[] memory) {
         return daoMembers[_daoMultiSigAddr];
     }
 
      // Function to get the number of members in a DAO
+      /**
+     * @dev Retrieves the number of members in a DAO associated with a specific multi-signature wallet address.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @return uint256 The number of members in the DAO.
+     */
     function getMemberCount(address _daoMultiSigAddr) public view returns (uint256) {
         return daoMembers[_daoMultiSigAddr].length;
     }
 
       // Function to get a specific member's details by phone number
+       /**
+     * @dev Retrieves a member's details by phone number from a specific DAO.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param _phoneNumber Phone number of the member to retrieve.
+     * @return MemberDetails Returns the member's details if found.
+     *         If the member is not found, it reverts with "Member with the specified phone number not found".
+     */
     function getMemberByPhoneNumber(address _daoMultiSigAddr, uint256 _phoneNumber) public payable  returns (MemberDetails memory) {
         MemberDetails[] storage members = daoMembers[_daoMultiSigAddr];
 
@@ -234,6 +289,12 @@ contract FullDaoContract {
         revert("Member with the specified phone number not found");
     }
        // Function to check if a specific address is a member of the DAO
+        /**
+     * @dev Checks if a specific address is a member of the DAO associated with a specific multi-signature wallet address.
+     * @param _memberToCheck Ethereum address of the member to check.
+     * @param _daoMultiSig Ethereum address of the DAO's multi-signature wallet.
+     * @return bool Returns true if the member is found, false otherwise.
+     */
     function isDaoMember(address _memberToCheck, address _daoMultiSig) public view returns (bool) {
         MemberDetails[] storage members = daoMembers[_daoMultiSig];
 
@@ -247,9 +308,16 @@ contract FullDaoContract {
     }
 
    
-   //Proposals 
-
-    // Function to add a proposal to a DAO
+   //Proposals --------------------------------------------------------------------------------------------------------------------------------------------------------------
+      /**
+     * @dev Function to add a proposal to a DAO.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param _pTitle Title of the proposal.
+     * @param _pSummary Summary of the proposal.
+     * @param _pDescription Description of the proposal.
+     * @param _duration Duration in seconds for the proposal to be active.
+     * return no return value.
+     */
     function addProposal(
         address _daoMultiSigAddr,
         string memory _pTitle,
@@ -276,8 +344,13 @@ contract FullDaoContract {
         // Add the new proposal to the DAO's proposal list
         daoProposals[_daoMultiSigAddr].push(newProposal);
     }
-
-    // Function to check if a proposal is expired by title
+    /**
+     * @dev Function to check if a proposal is expired by title.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param _pTitle Title of the proposal.
+     * @return bool Returns true if the proposal is expired, false otherwise.
+     *         If the proposal is not found, it reverts with "Proposal with the specified title not found".
+     */
     function isProposalExpiredByTitle(address _daoMultiSigAddr, string memory _pTitle) public view returns (bool) {
         ProposalDetails[] storage proposals = daoProposals[_daoMultiSigAddr];
 
@@ -291,6 +364,13 @@ contract FullDaoContract {
     }
 
      // Function to find proposal index by title or owner (can be implemented)
+       /**
+     * @dev Function to find the index of a proposal in a DAO's proposal list.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param _proposalOwner Ethereum address of the proposal owner.
+     * @return uint256 Returns the index of the proposal in the DAO's proposal list.
+     *         If the proposal is not found, it reverts with "Proposal not found".
+     */
     function findProposalIndex(address _daoMultiSigAddr, address _proposalOwner) internal view returns (uint256) {
         ProposalDetails[] storage proposals = daoProposals[_daoMultiSigAddr];
         for (uint256 i = 0; i < proposals.length; i++) {
@@ -301,14 +381,28 @@ contract FullDaoContract {
         revert("Proposal not found");
     }
 
-     // Function to check if a proposal is expired
+     // Function to check if a proposal is expired 
+         /**
+     * @dev Function to check if a proposal is expired.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param index Index of the proposal in the DAO's proposal list.
+     * @return bool Returns true if the proposal is expired, false otherwise.
+     *         If the index is out of bounds, it reverts with "Index out of bounds".
+     */
     function isProposalExpired(address _daoMultiSigAddr, uint256 index) public view returns (bool) {
         require(index < daoProposals[_daoMultiSigAddr].length, "Index out of bounds");
         ProposalDetails storage proposal = daoProposals[_daoMultiSigAddr][index];
         return block.timestamp > proposal.expirationTime; // Check if the current time is past the expiration time
     }
-
- // Function to cast a vote on a proposal
+      /**
+     * @dev Function to cast a vote on a proposal.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param _pTitle Title of the proposal.
+     * @param _voteType Type of vote (true for yes, false for no).
+     * return No return value.
+     * @dev The function checks if the caller is a DAO member and if the proposal is not expired.
+     *      If the conditions are met, a new vote is created and added to the proposal's vote list.
+     */
     function castVote(address _daoMultiSigAddr, string memory _pTitle, bool _voteType) public {
         require(isDaoMember(msg.sender, _daoMultiSigAddr), "Only DAO members can vote");
         require(!isProposalExpiredByTitle(_daoMultiSigAddr, _pTitle), "Proposal is expired");
@@ -316,17 +410,21 @@ contract FullDaoContract {
         // Create a new vote instance
         VoteDetails memory newVote = VoteDetails({
             voterAddr: msg.sender,
-            pOwner: findProposalOwner(_daoMultiSigAddr, _pTitle),
+            pOwner: findProposalOwnerByTitle(_daoMultiSigAddr, _pTitle),
             voteType: _voteType
         });
 
         // Add the new vote to the proposal's vote list
         proposalVotes[newVote.pOwner].push(newVote);
     }
-
-
-    // Function to find the proposal owner by title
-    function findProposalOwner(address _daoMultiSigAddr, string memory _pTitle) internal view returns (address) {
+    /**
+     * @dev Function to find the proposal owner by title.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param _pTitle Title of the proposal.
+     * @return address Returns the Ethereum address of the proposal owner.
+     *         If the proposal is not found, it reverts with "Proposal with the specified title not found".
+     */
+    function findProposalOwnerByTitle(address _daoMultiSigAddr, string memory _pTitle) internal view returns (address) {
         ProposalDetails[] storage proposals = daoProposals[_daoMultiSigAddr];
 
         for (uint256 i = 0; i < proposals.length; i++) {
@@ -336,18 +434,32 @@ contract FullDaoContract {
         }
         revert("Proposal with the specified title not found");
     }
-
-     // Function to get all votes for a specific proposal
+      /**
+     * @dev Function to retrieve all votes for a specific proposal.
+     * @param _proposalOwner Ethereum address of the proposal owner.
+     * @return VoteDetails[] An array of VoteDetails structs representing the votes for the proposal.
+     *         If no votes are found, an empty array is returned.
+     */
     function getVotes(address _proposalOwner) public view returns (VoteDetails[] memory) {
         return proposalVotes[_proposalOwner];
     }
-
-       // Function to get all proposals of a DAO
+        /**
+     * @dev Retrieves all proposals associated with a specific DAO.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @return ProposalDetails[] An array of ProposalDetails structs representing the proposals of the DAO.
+     *         If no proposals are found, an empty array is returned.
+     */
     function getProposals(address _daoMultiSigAddr) public view returns (ProposalDetails[] memory) {
         return daoProposals[_daoMultiSigAddr];
     }
 
-        // Function to get a specific proposal by title
+            /**
+     * @dev Retrieves a specific proposal by title from a specific DAO.
+     * @param _daoMultiSigAddr Address of the DAO's multi-signature wallet.
+     * @param _pTitle Title of the proposal.
+     * @return ProposalDetails Returns the details of the proposal if found.
+     *         If the proposal is not found, it reverts with "Proposal with the specified title not found".
+     */
     function getProposalByTitle(address _daoMultiSigAddr, string memory _pTitle) public view returns (ProposalDetails memory) {
         ProposalDetails[] storage proposals = daoProposals[_daoMultiSigAddr];
 
