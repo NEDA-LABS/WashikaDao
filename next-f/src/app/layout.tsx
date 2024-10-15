71ce6741bf4ba785aaa4271e3c73d2f858f5c573
clint911 <clingyking007@gmail.com>
2024-10-15 19:09:01 +0300

    adding everythng

---
+++ next-f/src/app/layout.tsx
@@ -0,0 +1,18 @@
+
+import "./globals.css";
+import { Provider } from "./provider";
+
+//host of RootLayout
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}
