b8d33b787939e56714b8b23388921443de86e09e
clint911 <clingyking007@gmail.com>
2024-10-16 13:14:55 +0300

    updating test create

--- Washika-dao-frontend/src/components/actions/CreateTestDao.tsx
+++ Washika-dao-frontend/src/components/actions/CreateTestDao.tsx
@@ -5,10 +5,14 @@ import { createDao } from "../../thirdweb/44787/0x50d92ea99af339519cd060c73d2db2
 import { useContract, useContractWrite, Web3Button } from "@thirdweb-dev/react";
 import { FDC_CAT_ABI } from "../../contractAbis/celoAlfajoresFullDaoContractAbi";
 
+import { prepareContractCall } from "thirdweb";
+import { useSendTransaction } from "thirdweb/react";
+
 export const TESTER_WALLET_AS_MSIG: Address = "0x31D5F5a357B4a9AD0b9107DC84eA8Cf7D0D621E7";
 
 export default function CreateTestDao(){
-  const TESTER_WALLET_AS_MSIG: Address = "0x31D5F5a357B4a9AD0b9107DC84eA8Cf7D0D621E7";
+
+ const TESTER_WALLET_AS_MSIG: Address = "0x31D5F5a357B4a9AD0b9107DC84eA8Cf7D0D621E7";
  const _daoName: string =   "TestingName";
  const _location: string = "KenyaTesting";
  const _targetAudience: string = "TestingTa";
@@ -18,24 +22,25 @@ export default function CreateTestDao(){
  const _daoImageUrlHash: string = "Imtheurl";
  const _multiSigAddr = TESTER_WALLET_AS_MSIG;
  const _multiSigPhoneNo  =  BigInt(724680615);
- const FULLDAO_CONTRACT_ADDRESS: Address = "0x50d92ea99af339519cd060c73d2db24620af2f57";
- const { contract } = useContract(FULLDAO_CONTRACT_ADDRESS);
-  const { mutateAsync, isLoading, error } =  useContractWrite(
-       contract,
-      "createDao",
-  );
 
+ const FULLDAO_CONTRACT_ADDRESS: Address = "0x50d92ea99af339519cd060c73d2db24620af2f57";
+
+ const { contract } = useContract(FULLDAO_CONTRACT_ADDRESS);
+
+ const { mutate: sendTransaction } = useSendTransaction();
+
+  const handleCreateDao = () => {
+    const transaction = prepareContractCall ({
+      contract,
+      method: "function createDao(string _daoName, string _location, string _targetAudience, string _daoTitle, string _daoDescription, string _daoOverview, string _daoImageUrlHash, address _multiSigAddr, uint256 _multiSigPhoneNo)",
+      params: [_daoName, _location, _targetAudience, _daoTitle, _daoDescription, _daoOverview, _daoImageUrlHash, _multiSigAddr, _multiSigPhoneNo]
+});
+         sendTransaction(transaction);
+  }
   return (
   <>
-      <Web3Button
-        contractAddress={FULLDAO_CONTRACT_ADDRESS}
-      action={() => mutateAsync({
-        args: [
-           _daoName, _location, _targetAudience, _daoTitle, _daoDescription, _daoOverview, _daoImageUrlHash, _multiSigAddr, _multiSigPhoneNo
-        ]
-      }) }
-      > SendCreateDaoTransaction</Web3Button>
-  </>
+      <button onClick={handleCreateDao}>Create A Dao</button>
+ </>
   )
 }
 
