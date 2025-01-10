23024e89271443ab5ddd2c8707dbbbe6ec3b4ba9
clint911 <clingyking007@gmail.com>
2025-01-10 23:30:03 +0300

    updating  dao registration

--- Washika-dao-frontend/src/components/actions/CreateDao.tsx
+++ Washika-dao-frontend/src/components/actions/CreateDao.tsx
@@ -1,13 +1,13 @@
-import { useActiveAccount, useActiveWallet } from "thirdweb/react";
-import { FormData } from "../../pages/DaoRegistration";
-import { useState } from "react";
+//import { useActiveAccount, useActiveWallet } from "thirdweb/react";
+//import { FormData } from "../../pages/DaoRegistration";
+//import { useState } from "react";
 export default function CreateDao(){
 //Creating Dao using data from  DaoForm
-const wallet = useActiveWallet();
-console.log(wallet);//Cross Checking the wallet
-//checking & grabbing the active account 
-const activeAcc = useActiveAccount(); //str or undefined 
-const [isUsrLoggedIn, setIsUsrLoggedIn] = useState<boolean>(false); 
+//const wallet = useActiveWallet();
+//console.log(wallet);//Cross Checking the wallet
+//checking & grabbing the active account
+//const activeAcc = useActiveAccount(); //str or undefined
+//const [isUsrLoggedIn, setIsUsrLoggedIn] = useState<boolean>(false);
 
 }
 
