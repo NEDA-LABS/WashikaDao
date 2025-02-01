f073b0d96712d81fd2fed97f8ddcfdabb323795b
clint911 <clingyking007@gmail.com>
2025-02-02 02:48:38 +0300

    updating migrations

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -21,14 +21,13 @@ import path from "path";
 const isProduction = process.env.NODE_ENV === 'production';
 
  const AppDataSource = new DataSource({
-  type: isProduction ? 'postgres' : 'sqlite', // Use PostgreSQL in production, SQLite in development
-  url: isProduction ? process.env.DATABASE_URL : undefined, // Use DATABASE_URL in production
-  database: isProduction ? undefined : './mydevdb.sqlite', // SQLite database file for development
-  synchronize: !isProduction, // Disable synchronize in production (use migrations instead)
+  type:  'postgres', // Use PostgreSQL in production, SQLite in development
+  url:  process.env.DATABASE_URL, // Use DATABASE_URL in production
+  synchronize: false, // Disable synchronize in production (use migrations instead)
   logging: true, // Enable logging only in development
   entities: [Dao, MemberDetails, Proposal, Vote], // Your entities
   migrations: [path.join(__dirname, './migrations/*{.ts,.js}')], // Migration files
-  migrationsRun: isProduction, // Automatically run migrations in production
+  migrationsRun: true, // Automatically run migrations in production
   extra: isProduction
     ? {
         ssl: {
