4443e4552b3c66171477437fcaf1d9bb21f603d7
Christopher <mwanikigachina@gmail.com>
2025-02-20 00:50:31 +0300

    update mobile navigation menu

--- Washika-dao-frontend/src/pages/SuperAdmin.tsx
+++ Washika-dao-frontend/src/pages/SuperAdmin.tsx
@@ -63,7 +63,6 @@ const SuperAdmin: React.FC = () => {
           }
         );
         const data = await response.json();
-        console.log(data);
 
         if (response.ok) {
           setDaoDetails(data.daoDetails);
