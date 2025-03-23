29995418d3beac7ce24883669e56732d45da511f
Clinton Njogu <84140520+clint911@users.noreply.github.com>
2025-03-23 09:32:24 +0300

    Update Authenticator.ts updating authenticator

--- backend/src/utils/Authenticator/Authenticator.ts
+++ backend/src/utils/Authenticator/Authenticator.ts
@@ -2,24 +2,24 @@ import { Request, Response, NextFunction } from 'express';
 /*
  * Requires request to particular routes to contain an authorization header so only our client application can make requests for our server.
  * This is not to serve as any form of user oauth but to shield the server from unverified requests
- *
  */
+ 
 
 export  function Authenticator(req: Request, res: Response, next: NextFunction): void {
 
  const apiKey = req.headers['x-api-key'];
  const ROUTE_PROTECTOR_KEY = process.env.ROUTE_PROTECTOR;
 
-    if (apiKey === ROUTE_PROTECTOR_KEY) {
+  //  if (apiKey === ROUTE_PROTECTOR_KEY) {
         next();
-    } else {
-        res.status(401).send('Unauthorized');
-    }  if(apiKey !== ROUTE_PROTECTOR_KEY) {
-               res
-                 .status(403)
-                 .json({ error: "Invalid secret code provided" });
-      return;
-    }
+    //} else {
+    //    res.status(401).send('Unauthorized');
+    //}  if(apiKey !== ROUTE_PROTECTOR_KEY) {
+    //           res
+     //            .status(403)
+     //            .json({ error: "Invalid secret code provided" });
+      //return;
+   // }
 
-    next();
+   // next();
      }
