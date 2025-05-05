7000c4e0e94631f2511c1bdc85f66b6670a3f952
Christopher <mwanikigachina@gmail.com>
2025-05-05 19:43:36 +0300

    update type

--- Washika-dao-frontend/src/utils/Types.ts
+++ Washika-dao-frontend/src/utils/Types.ts
@@ -59,7 +59,7 @@ export interface IBackendDaoMember {
   nationalIdNo: string;
   memberRole: DaoRoleEnum | string;
   memberCustomIdentifier: string;
-  memberAddr: string;
+  memberAddr?: string;
 }
 
 
