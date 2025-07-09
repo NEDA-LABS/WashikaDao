f8e53d5d475af244223b33eb8192467873c58505
clint911 <clingyking007@gmail.com>
2025-07-09 19:36:57 +0300

    updating vite config for build

---
+++ Washika-dao-frontend/vite.config.prod.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["all"],
+  },
+  define: {
+    'process.env.VITE_THIRDWEB_CLIENT_ID': JSON.stringify(process.env.VITE_THIRDWEB_CLIENT_ID)
+  }
+}); 
